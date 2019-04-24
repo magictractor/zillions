8aa275004995d989801366c8fcbb3245ffe481ea
Ken D <git@magictractor.co.uk>
2019-04-24 09:27:21 +0100

    Work in progress - benchmarks and displaying strategy info

--- zillions-testbed/src/main/java/uk/co/magictractor/zillions/benchmark/arithmetic/MultiplyBenchmark.java
+++ zillions-testbed/src/main/java/uk/co/magictractor/zillions/benchmark/arithmetic/MultiplyBenchmark.java
@@ -17,6 +17,10 @@ package uk.co.magictractor.zillions.benchmark.arithmetic;
 
 import static uk.co.magictractor.zillions.core.environment.Environment.getBestAvailableImplementation;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -31,29 +35,45 @@ public class MultiplyBenchmark {
 //		super(BigInt::multiply);
 //	}
 
+	// helps - but still a hit on the first test - ah! recorded time ok
+	// the time reported in JUnit is wonky (can it be altered?)
+	@BeforeAll
 	public static void warmUp() {
-		getBestAvailableImplementation(RandomStrategy.class).setSeed(0L);
-		BigInt x = BigIntFactory.random(0);
+		//getBestAvailableImplementation(RandomStrategy.class).setSeed(0L);
+		//BigInt x = BigIntFactory.random(0);
+		BigInt x = BigIntFactory.from(0);
 		x.multiply(x);
 	}
-	
+
 	// TODO! standardised random numbers
 	@ParameterizedTest
-	@ValueSource(ints = { 50000, 100000, 150000, 200000 })
+	// 1 for warming up
+	@ValueSource(ints = { 1, 50000, 100000, 150000, 200000 })
 	public void testMultiplyParameterised(int numBits) {
-		BigInt x = BigIntFactory.random(numBits-1).setBit(numBits);
-		BigInt y = BigIntFactory.random(numBits-1).setBit(numBits);
+		BigInt x = BigIntFactory.random(numBits - 1).setBit(numBits);
+		BigInt y = BigIntFactory.random(numBits - 1).setBit(numBits);
 		long t = recordTime(() -> x.multiply(y));
-		
-		System.out.println(numBits + " multiplication -> " + t);
+
+		System.out.println(numBits + " multiplication -> " + t/1.0e9);
 	}
-	
+
 	private long recordTime(Runnable runnable) {
+		ThreadMXBean mx = ManagementFactory.getThreadMXBean();
+
+		// mx.setThreadCpuTimeEnabled(true);
+		System.err.println("isCurrentThreadCpuTimeSupported: " + mx.isCurrentThreadCpuTimeSupported());
+		System.err.println("isThreadCpuTimeEnabled: " + mx.isThreadCpuTimeEnabled());
+
+		// long before = mx.getCurrentThreadUserTime();
+		// long before = mx.getCurrentThreadCpuTime();
 		long before = System.nanoTime();
 		runnable.run();
+		// long after = mx.getCurrentThreadUserTime();
+		// long after = mx.getCurrentThreadCpuTime();
 		long after = System.nanoTime();
-		
+
 		return after - before;
+		// hmm occassionally 15625000, usually zero (both user time and cpu time)
 	}
 
 }
---
+++ zillions-testbed/src/main/java/uk/co/magictractor/zillions/testbed/importer/StrategiesUtil.java
@@ -0,0 +1,20 @@
+package uk.co.magictractor.zillions.testbed.importer;
+
+import java.util.List;
+
+import uk.co.magictractor.zillions.core.environment.Environment;
+import uk.co.magictractor.zillions.core.environment.StrategyHolder;
+
+public final class StrategiesUtil {
+
+	private StrategiesUtil() {
+	}
+
+	public static <T> void dumpStrategyInfo(Class<T> apiClass) {
+		List<StrategyHolder<T>> infos = Environment.getStrategyList(apiClass).strategyHolders();
+
+		for (StrategyHolder<T> info : infos) {
+			System.err.println(info.getStrategy() + "  " + info.isAvailable() + "  " + info.getPriority());
+		}
+	}
+}
