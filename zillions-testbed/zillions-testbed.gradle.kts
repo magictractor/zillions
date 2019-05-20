e70220f8073810c506f6e68b764e22bbe4ba39db
Ken D <git@magictractor.co.uk>
2019-05-20 10:37:59 +0100

    Remove dependency on JUnit4

--- zillions-testbed/zillions-testbed.gradle.kts
+++ zillions-testbed/zillions-testbed.gradle.kts
@@ -4,8 +4,9 @@ plugins {
 
 dependencies {
     api(project(":zillions-core"))
+    // junit-jupiter-params for @ParameterizedTests
     api("org.junit.jupiter:junit-jupiter-params:5.4.2")
-    api("org.junit.platform:junit-platform-suite-api:1.4.1")
-    api("org.junit.platform:junit-platform-runner:1.4.1")
+    api("org.junit.platform:junit-platform-suite-api:1.4.2")
+    implementation("org.junit.platform:junit-platform-launcher:1.4.2")
     api("org.assertj:assertj-core:3.11.1")
 }
